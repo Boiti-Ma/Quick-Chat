435bdd310bc2775d5802134e37aff6207d52a4fe
Student <Student@LabVM2047644.e2po12bhjmrede2v0u3hhphhfd.jnbx.internal.cloudapp.net>
2026-06-14 15:50:45 +0200

    Added a JSON file method to store messages

--- src/main/java/com/mycompany/quick/chat/Message_Text.java
+++ src/main/java/com/mycompany/quick/chat/Message_Text.java
@@ -11,16 +11,32 @@ package com.mycompany.quick.chat;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.json.JSONObject;
+import java.util.Scanner;
 class Message{
+    static int max=100;
+    // Array for storing
+   static String[] messageIDs=new String[max];
+   static String[] messageHashes=new String[max];
+  static  String[] disregardMessages=new String[max];
+   static String[] StoredMessages=new String[max];
+   static String[] SentMessages=new String[max];
+   static String[] recipient=new String[max];
+   
+    //Static String[] sentMessages=new String[100]
+    static int totalMessages=0;
+    static int disregardedCount=0;
+    static int SentMessagesCount=0;
+    static Scanner scan =new Scanner(System.in);
+    
     private String messageID;
   private int messageNumber;
-     private String recipient;
+     private String recipients;
      private String message;
      private String messageHash;
      
      //Creating a constructor
      public Message(String recipient,String message){
-         this.recipient=recipient;
+         this.recipients=recipient;
          this.message=message;
          this.messageNumber=messageNumber;
          this.messageID="1234567890";
@@ -35,7 +51,7 @@ class Message{
      }
      // check recipient cell
      public boolean checkRecipientCell(){
-         if(recipient.startsWith("+27")&& recipient.length()<=12){
+         if(recipients.startsWith("+27")&& recipients.length()<=12){
              return true;
          }else{
              return false;
@@ -61,8 +77,115 @@ class Message{
             FileWriter file = new FileWriter("messages.json");
             file.write(obj.toString(4)); // Pretty format
             file.close();
+            
          return obj;
      }
+     public static void disregardMessages(){
+         System.out.println("Enter Message to disregard");
+         String msg=scan.nextLine();
+         disregardMessages[disregardedCount++]=msg;
+         System.out.println("Message disregarded");
+     }
+     public static void StoreMessagemenue(){
+         int option;
+         do{
+             System.out.println("1. Display Sender and Recipient");
+        System.out.println("2. Display Longest Message");
+        System.out.println("3. Search by Message ID");
+        System.out.println("4. Search by Recipient");
+        System.out.println("5. Delete Message by Hash");
+        System.out.println("6. Display Full Report");
+    option=scan.nextInt();
+    scan.nextLine();
+    switch(option){
+        case 1:
+            DisplaySenderandRecipient();
+            break;
+
+            case 2:
+                DisplayLongestMessage();
+                break;
+                case 3:
+                    SearchbymessageID(scan.nextLine()); 
+                    break;
+                    case 4:
+                        SearchbymessageID(scan.nextLine());
+                        break;
+                        case 5:
+                            SearchbyRecipient(scan.nextLine()); 
+                            break;
+                        default:
+                            System.out.println("SELECTED INVALID OPTION");
+    }
+         }while(option!=5);
+     }
+     public static void DisplaySenderandRecipient(){
+         if(SentMessagesCount==0){
+             System.out.println("No stored messages");
+         }
+         for(int i=0;i<SentMessagesCount;i++){
+             System.out.println("Recipient"+ recipient[i]);
+         }
+     }
+     public static void DisplayLongestMessage(){
+         if (SentMessagesCount == 0) {
+        System.out.println("No stored Messages");
+    }
+
+    int longest = 0;
+    for (int i = 1; i < SentMessagesCount; i++) {
+        if (StoredMessages[i].length() > StoredMessages[longest].length()) {
+            longest = i;
+        }
+    }
+
+    System.out.println("\nLongest Message");
+    System.out.println("ID" + messageIDs[longest]);
+    System.out.println("Recipient" + recipient[longest]);
+    System.out.println("Message" + StoredMessages[longest]);
+}
+     
+     // Search by Message ID
+public static void SearchbymessageID(String id) {
+    for (int i = 0; i < SentMessagesCount; i++) {
+        if (messageIDs[i].equalsIgnoreCase(id)) {
+            System.out.println("Recipient" + recipient[i]);
+            System.out.println("Mesage" + StoredMessages[i]);
+        }
+    }
+}
+
+// Search by Recipient
+public static void SearchbyRecipient(String recipients) {
+    boolean found = false;
+    for (int i = 0; i < SentMessagesCount; i++) {
+        if (recipient[i].equalsIgnoreCase(recipients)) {
+            System.out.println("\nID" + messageIDs[i]);
+            System.out.println("Recipient" + recipient[i]);
+            System.out.println("Mesage" + StoredMessages[i]);
+            found = true;
+        }
+    }
+    if (!found) {
+        System.out.println("No Messages Found");
+    }
+}
+
+// Delete Message by Hash
+public static void DeleteMessagebyHash(String Hash) {
+    for (int i = 0; i < SentMessagesCount; i++) {
+        if (messageHashes[i].equalsIgnoreCase(Hash)) {
+            for (int j = i; j < SentMessagesCount; j++) {
+                messageIDs[j] = messageIDs[j + 1];
+                messageHashes[j] = messageHashes[j + 1];
+                recipient[j] = recipient[j + 1];
+                StoredMessages[j] = StoredMessages[j + 1];
+            }
+            SentMessagesCount--;
+            System.out.println("Message deleted successfully.");
+        }
+    }
+}
      //Create Message Hash
      public String createMessageHash(){
          String[] words= message.split(" ");
---
+++ src/test/java/com/mycompany/quick/chat/MessageIT.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.quick.chat;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class MessageIT {
+    
+    public MessageIT() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of checkMessageID method, of class Message.
+     */
+    @Test
+    public void testCheckMessageID() {
+        System.out.println("checkMessageID");
+        Message instance = null;
+        boolean expResult = false;
+        boolean result = instance.checkMessageID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of checkRecipientCell method, of class Message.
+     */
+    @Test
+    public void testCheckRecipientCell() {
+        System.out.println("checkRecipientCell");
+        Message instance = null;
+        boolean expResult = false;
+        boolean result = instance.checkRecipientCell();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sentMessage method, of class Message.
+     */
+    @Test
+    public void testSentMessage() {
+        System.out.println("sentMessage");
+        Message instance = null;
+        String expResult = "";
+        String result = instance.sentMessage();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Storemessage method, of class Message.
+     */
+    @Test
+    public void testStoremessage() throws Exception {
+        System.out.println("Storemessage");
+        Message instance = null;
+        JSONObject expResult = null;
+        JSONObject result = instance.Storemessage();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of disregardMessages method, of class Message.
+     */
+    @Test
+public void testDisregardMessages() {
+    String recipient = "+27834484567";
+    String message = "Yohoooo, I am at your gate.";
+    String flag = "Disregard";
+
+    assertEquals("Disregard", flag);
+}
+
+@Test
+public void testStoreMessagemenue() {
+    String recipient = "+27838884567";
+    String message = "Where are you? You are late! I have asked you to be on time.";
+    String flag = "Stored";
+
+    assertEquals("Stored", flag);
+}
+
+@Test
+public void testDisplaySenderandRecipient() {
+    String recipient = "+27834557896";
+    String message = "Did you get the cake?";
+
+    assertEquals("+27834557896", recipient);
+}
+
+@Test
+public void testDisplayLongestMessage() {
+    String longestMessage =
+        "Where are you? You are late! I have asked you to be on time.";
+
+    assertEquals(
+        "Where are you? You are late! I have asked you to be on time.",
+        longestMessage
+    );
+}
+
+@Test
+public void testSearchbymessageID() {
+    String recipient = "+27834557896";
+    String message = "Did you get the cake?";
+    String flag = "Sent";
+
+    assertNotNull(message);
+}
+
+@Test
+public void testSearchbyRecipient() {
+    String recipient = "+27838884567";
+    String message = "Ok, I am leaving without you.";
+    String flag = "Stored";
+
+    assertEquals("+27838884567", recipient);
+}
+
+    /**
+     * Test of DeleteMessagebyHash method, of class Message.
+     */
+    @Test
+    public void testDeleteMessagebyHash() {
+        System.out.println("DeleteMessagebyHash");
+        String Hash = "";
+        Message.DeleteMessagebyHash(Hash);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of createMessageHash method, of class Message.
+     */
+    @Test
+    public void testCreateMessageHash() {
+        System.out.println("createMessageHash");
+        Message instance = null;
+        String expResult = "";
+        String result = instance.createMessageHash();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of printMessage method, of class Message.
+     */
+    @Test
+    public void testPrintMessage() {
+        System.out.println("printMessage");
+        Message instance = null;
+        String expResult = "";
+        String result = instance.printMessage();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
